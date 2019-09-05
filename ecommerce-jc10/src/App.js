61c477bc8a599bf0f77e56158e71aeb8838e4ab1
tmangowal <tmangowal@gmail.com>
2019-09-05 23:56:57 +0700

    finished answers

--- ecommerce-jc10/src/App.js
+++ ecommerce-jc10/src/App.js
@@ -1,56 +1,16 @@
 import React, {Component} from 'react';
-import {withRouter, Route, Switch} from 'react-router-dom'
-import Home from './1.pages/Home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import NavbarComp from './1.pages/Navbar/Navbar';
-import Auth from './1.pages/Auth/Auth';
-import Cookie from 'universal-cookie'
-import {connect} from 'react-redux'
-import {keepLogin, cookieChecker, getCartLength} from './redux/1.actions'
-import ProductDetails from './1.pages/ProductDetails/ProductDetails';
-import Cart from './1.pages/Cart/Cart';
-import AdminDashboard from './1.pages/Admin/AdminDashboard';
-import HistoryComp from './1.pages/History/History'
-import Wishlist from './1.pages/Wishlist/Wishlist';
 
-
-let cookieObj = new Cookie()
 class App extends Component {
 
-  componentDidMount(){
-    let cookieVar = cookieObj.get('userData')
-    if(cookieVar){
-      this.props.keepLogin(cookieVar)
-    }else{
-      this.props.cookieChecker()
-    }
-  }
-
   render(){
-    if(this.props.globalCookie){
       return (
         <div>
-          <NavbarComp/>
-          <Switch>
-            <Route component={Home} path='/' exact />
-            <Route component={Auth} path='/auth' exact />
-            <Route component={ProductDetails} path='/product-details/:id' exact />
-            <Route component={Cart} path='/cart' exact />
-            <Route component={HistoryComp} path='/history' exact />
-            <Route component={Wishlist} path='/wishlist' exact />
-            <Route component={AdminDashboard} path='/admin/dashboard' exact />
-          </Switch>
+          loh kok nyontek hehe
         </div>
       )
-    }
-    return <div>Loading ...</div>
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    globalCookie : state.user.cookie
-  }
-}
 
-export default connect(mapStateToProps, {keepLogin, cookieChecker, getCartLength})(withRouter(App))
+
+export default App
--- ecommerce-jc10/src/redux/2.reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-import userGlobal from './userGlobal';
-import cartGlobal from './cartGlobal';
-
-
-export default combineReducers({
-    user : userGlobal,
-    cart : cartGlobal
-})
